8e2c2a4724df9df199a8027672f69fcf9b1a051f
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2022-02-22 12:46:55 +0530

    First Commit

--- Implementation/109A.cpp
+++ Implementation/109A.cpp
@@ -4,10 +4,28 @@ int main(){
     int n;
     cin>>n;
     string s;
+    char se = '7';
+    char f = '4';
     int sum = 0;
-    while (sum<n)
+    while (n>0)
     {
-        
+        if(n>7){
+            n= n-7;
+            s = s+se;
+
+        }
+        else{
+            n= n-4;
+            if(n==0){
+                s = s+ f;
+            }
+        }
+    }
+    if(n==0){
+    cout<<s;
+    }else
+    {
+        cout<<-1;
     }
     
     
